34f8680351fbe8e4c54b00eb048f67d7d35e6dc7
Harehn <harehn.kaundun@mail.mcgill.ca>
2020-02-11 19:45:07 -0500

    Completed argument parser

--- Main.java
+++ Main.java
@@ -2,8 +2,51 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(args.length);
+    int timeout = 5;
+    boolean t_flag = false;
+    int max_retries = 3;
+    boolean r_flag = false;
+    int port = 53;
+    boolean port_flag = false;
+    String query_type = "type A"; 
+    String server = "";
+    String name = "";
     
+    
+    
+    int i = 0; 
+    if(args[i]=="-t") {
+      i++;
+      timeout = Integer.parseInt(args[i]);
+      i++;
+      t_flag = true;
+    }
+    
+    if(args[i]=="-r") {
+      i++;
+      max_retries = Integer.parseInt(args[i]);
+      i++;
+      r_flag = true;
+    }    
+    
+    if(args[i]=="-p") {
+      i++;
+      port = Integer.parseInt(args[i]);
+      i++;
+      port_flag = true;
+    }
+    
+    if(args[i]=="-mx") {
+      query_type = "mail Server";
+      i++;
+    }else if(args[i]=="-ns") {
+      query_type = "name server";
+      i++;
+    }
+    
+    server = args[i].substring(1);
+    i++;
+    name = args[i];
   }
   
   public void throwError() {
